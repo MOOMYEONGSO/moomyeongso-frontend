70bf1ae77ee7b5bcea4a5e353a7dd927be59e108
respectwo2 <respectwo@naver.com>
2026-03-27 11:41:16 +0900

    feat: GA & SPA 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,26 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import { useEffect } from "react";
 import { attachGlobalFullscreenListenersOnce } from "./lib/fullscreen/fullscreenStore";
+import { initGa, trackPageView } from "./lib/analytics/ga";
 
 function App() {
   useEffect(() => {
     attachGlobalFullscreenListenersOnce();
+
+    initGa();
+    const getCurrentPath = () =>
+      `${router.state.location.pathname}${router.state.location.search}`;
+
+    trackPageView(getCurrentPath());
+    const unsubscribe = router.subscribe((state) => {
+      trackPageView(`${state.location.pathname}${state.location.search}`);
+    });
+
+    return () => {
+      unsubscribe();
+    };
   }, []);
+
   return <RouterProvider router={router} />;
 }
 
